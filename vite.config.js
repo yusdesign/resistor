3959b7b00784a3d8dafcefcad9f927c3df8800e0
yusdesign <swiftcase@gmail.com>
2026-06-29 01:27:12 +0800

    Icons 📻

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
+import { copy } from 'vite-plugin-copy'; // npm install vite-plugin-copy
 
 export default defineConfig({
+  plugins: [
+    copy({
+      targets: [
+        { src: 'src/icons/*', dest: 'dist/icons' }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
     assetsDir: '.',
